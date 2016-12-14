b09e4cf3fff212d28525357eab2e3085fd6f9264
Erlend Aakre <erlend.aakre@momogroup.co>
2016-12-14 13:47:48 +0000

    41 - pandigital prime implemented but not verified

---
+++ WIP_041.scala
@@ -0,0 +1,4 @@
+val primes: Stream[Int] = 2 #:: Stream.from(3, 2).filter(i => primes.takeWhile(j => j * j <= i).forall(k => i % k > 0))
+def pandigital(n: Int, digits: Int) = n.toString.length == digits && (1 to digits).forall(d => n.toString.contains(d.toString))
+
+primes.takeWhile(p => p < 987654321).filter(pandigital(_,9)).max
