b761afb7d6c8e19955882fe6f913ede2bbf70a9f
Erlend Aakre <erlend.aakre@momogroup.co>
2016-12-14 15:22:06 +0000

    41: out of memory :/

--- WIP_041.scala
+++ WIP_041.scala
@@ -2,3 +2,4 @@ val primes: Stream[Int] = 2 #:: Stream.from(3, 2).filter(i => primes.takeWhile(j
 def pandigital(n: Int, digits: Int) = n.toString.length == digits && (1 to digits).forall(d => n.toString.contains(d.toString))
 
 primes.takeWhile(p => p < 987654321).filter(pandigital(_,9)).max
+// java.lang.OutOfMemoryError: GC overhead limit exceeded
